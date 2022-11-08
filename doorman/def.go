db40673350e21b6510a8bc1a2b00c1da110437a8
swskipper <skipper@southwinds.io>
2022-11-08 17:04:46 +0000

    add doorman configuration types

---
+++ doorman/def.go
@@ -0,0 +1,16 @@
+/*
+  types - © 2018-Present - SouthWinds Tech Ltd - www.southwinds.io
+  Licensed under the Apache License, Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0
+  Contributors to this project, hereby assign copyright in this code to the project,
+  to be licensed under the same terms as the rest of the code.
+*/
+
+package doorman
+
+const (
+	PipelineType      = "DOORMAN-PIPELINE"
+	InboundRouteType  = "DOORMAN-INBOUND-ROUTE"
+	OutboundRouteType = "DOORMAN-OUTBOUND-ROUTE"
+	NotificationType  = "DOORMAN-NOTIFICATION"
+	CommandType       = "DOORMAN-COMMAND"
+)
